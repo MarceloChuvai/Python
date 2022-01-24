b893db562828f237462269eb4535e8480deda2d4
Marcelo Chuvai <77127403+MarceloChuvai@users.noreply.github.com>
2022-01-24 17:12:33 -0300

    Create cotacao.py

---
+++ cotacao.py
@@ -0,0 +1,44 @@
+import json
+import requests
+
+req = None
+
+def requisicao(titulo):     # USD-BRL,EUR-BRL,BTC-BRL
+    try:
+        req = requests.get("https://economia.awesomeapi.com.br/last/" + titulo)
+        iRETORNO = json.loads(req.text)
+        return iRETORNO
+
+    except Exception as e:
+        print('Não foi possível fazer a requisição', e)
+        return None
+
+def descompactar(conteudo, cont):
+    descompactado = conteudo[cont]
+    return descompactado
+
+
+def printa_detalhe(descompactado):
+    print('Nome:', descompactado['name'])
+    print('Alta:', descompactado['high'])
+    print('Baixa:', descompactado['low'])
+    print('Variação hoje:', descompactado['varBid'])
+    print('Porcentagem de Variação:', descompactado['pctChange'])
+    print('Dia e hora da requisição:', descompactado['create_date'])
+    print('Valor de Compra:', descompactado['bid'])
+    print('Valor para Venda:', descompactado['ask'])
+
+moeda = input('Digite uma moeda a ser consultada:\nOpções:\n\n"USD-BRL, EUR-BRL, BTC-BRL": ')
+valor = requisicao(moeda)
+
+if moeda == 'USD-BRL':
+    cont = 'USDBRL'
+    des = descompactar(valor, cont)
+if moeda == 'EUR-BRL':
+    cont = 'EURBRL'
+    des = descompactar(valor, cont)
+if moeda == 'BTC-BRL':
+    cont = 'BTCBRL'
+    des = descompactar(valor, cont)
+
+printa_detalhe(des)
